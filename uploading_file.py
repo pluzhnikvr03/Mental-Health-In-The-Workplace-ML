e2465959669cebd460448bed7863b03d3eadaf15
Victoria <150126357+pluzhnikvr03@users.noreply.github.com>
2026-05-29 12:19:35 +0300

    Create uploading_file.py

---
+++ uploading_file.py
@@ -0,0 +1,5 @@
+# Разведочный анализ (EDA) и подготовка данных
+url = "https://raw.githubusercontent.com/pluzhnikvr03/Black-Friday-ML-/refs/heads/main/mental_health_workplace.csv"
+data = pd.read_csv(url)
+
+data.head()
